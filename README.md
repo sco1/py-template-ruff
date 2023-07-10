0407b1066cb301dc7f7bfc0182f717948a796096
S. Co1 <sco1.git@gmail.com>
2023-07-10 14:58:52 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# py-template
+[![pre-commit](https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit&logoColor=white)](https://github.com/pre-commit/pre-commit)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-black)](https://github.com/psf/black)
+
+It's a Python project template!
---
+++ run_me.py
@@ -0,0 +1,45 @@
+import typing as t
+from pathlib import Path
+
+BASE_NAME = "py-template"
+BASE_DESCRIPTION = "It's a Python project template!"
+BASE_NAME_UNDERSCORE = BASE_NAME.replace("-", "_")
+
+
+class TemplateSwap(t.NamedTuple):  # noqa: D101
+    src: Path
+    needs_hyphen: bool = False
+    has_description: bool = False
+
+
+SWAPS = (
+    TemplateSwap(src=Path("./.ruff.toml")),
+    TemplateSwap(src=Path("./pyproject.toml"), needs_hyphen=True, has_description=True),
+    TemplateSwap(src=Path("./README.md"), needs_hyphen=True, has_description=True),
+    TemplateSwap(src=Path("./tox.ini")),
+)
+
+
+def make_swaps(new_name: str, new_description: str) -> None:  # noqa: D103
+    underscored_name = new_name.replace("-", "_")
+
+    for swap in SWAPS:
+        full_src = swap.src.read_text()
+
+        if swap.needs_hyphen:
+            full_src = full_src.replace(BASE_NAME, new_name)
+        else:
+            full_src = full_src.replace(BASE_NAME_UNDERSCORE, underscored_name)
+
+        if swap.has_description:
+            full_src = full_src.replace(BASE_DESCRIPTION, new_description)
+
+        swap.src.write_text(full_src)
+
+
+if __name__ == "__main__":
+    new_name = input("New Package Name (hyphenated): ")
+    new_description = input("New Package Description: ")
+
+    make_swaps(new_name, new_description)
+    Path(__file__).unlink()
